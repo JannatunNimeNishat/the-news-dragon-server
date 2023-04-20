c09e46ba0a4226ca0c17c2bff47a67ef6d68b51a
JannatunNimeNishat <nishatnime100@gmail.com>
2023-04-20 23:48:02 +0600

    all news news by category and specific id API added

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 const port = process.env.PORT ||  5000;
 //categories
 const categories = require('./data/categories.json');
+//news
+const news = require('./data/news.json')
+
+
+
 //cors
 const cors = require('cors')
 
@@ -17,6 +22,25 @@ app.get('/categories',(req,res)=>{
     res.send(categories);
 })
 
+//all news
+app.get('/news',(req,res)=>{
+    res.send(news);
+})
+
+// single news
+app.get('/news/:id',(req,res)=>{
+    const id = req.params.id;
+    // console.log(id, typeof id);
+    const singleNews = news.find(singleNews => singleNews._id === id);
+    res.send(singleNews)
+})
+
+//category wise news
+app.get('/category/:id',(req,res)=>{
+    const id = req.params.id;
+    const category_wise_news = news.filter(categoryNews => categoryNews.category_id === id);
+    res.send(category_wise_news);
+})
 
 app.listen(port, ()=>{
     console.log(`Dragon news API is running on Port: ${port}`)
