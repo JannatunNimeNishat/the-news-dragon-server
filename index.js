98459216afd9a1c482ec5713277f01fbb4225d21
JannatunNimeNishat <nishatnime100@gmail.com>
2023-04-21 00:52:54 +0600

    show all news condition added on catagory wise news API

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const port = process.env.PORT ||  5000;
+const port = process.env.PORT || 5000;
 //categories
 const categories = require('./data/categories.json');
 //news
@@ -13,22 +13,22 @@ const cors = require('cors')
 
 app.use(cors());
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Dragon News is running');
 })
 
 //categories
-app.get('/categories',(req,res)=>{
+app.get('/categories', (req, res) => {
     res.send(categories);
 })
 
 //all news
-app.get('/news',(req,res)=>{
+app.get('/news', (req, res) => {
     res.send(news);
 })
 
 // single news
-app.get('/news/:id',(req,res)=>{
+app.get('/news/:id', (req, res) => {
     const id = req.params.id;
     // console.log(id, typeof id);
     const singleNews = news.find(singleNews => singleNews._id === id);
@@ -36,13 +36,19 @@ app.get('/news/:id',(req,res)=>{
 })
 
 //category wise news
-app.get('/category/:id',(req,res)=>{
+app.get('/category/:id', (req, res) => {
     const id = req.params.id;
-    const category_wise_news = news.filter(categoryNews => categoryNews.category_id === id);
-    res.send(category_wise_news);
+    if (id === '0') {
+        res.send(news);
+    }
+    else {
+        const category_wise_news = news.filter(categoryNews => categoryNews.category_id === id);
+        res.send(category_wise_news);
+
+    }
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Dragon news API is running on Port: ${port}`)
 })
 
